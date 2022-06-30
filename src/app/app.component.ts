87c18ef451fc286deaea1784cd977e4b0bee01bd
navneetk926 <84176222+navneetk926@users.noreply.github.com>
2022-06-30 15:51:05 +0530

    Create app.component.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './trade.component.html',
+  styleUrls: ['./trade.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent {
+  title = 'simple-side-nav';
+}
