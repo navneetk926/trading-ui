57eb019b5cf819eabc20e47b0a4b264bd0b2002d
navneetk926 <84176222+navneetk926@users.noreply.github.com>
2022-06-30 15:52:24 +0530

    Create trade.component.ts

---
+++ src/app/pages/trade/trade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ccapi',
+  templateUrl: './ccapi.component.html',
+  styleUrls: ['./ccapi.component.scss']
+})
+export class TradeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
